bd93030d9e9e5f42b4def8119f01d917e4d131b9
t.hinkov23 <t.hinkov23@LABSCIFIPC04>
2023-04-25 13:39:27 +0300

    Add project files.

---
+++ HinkovT_FinalPr_Sem2/Fleet.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HinkovT_FinalPr_Sem2
+{
+    /// <summary>
+    /// Interaction logic for Fleet.xaml
+    /// </summary>
+    public partial class Fleet : Window
+    {
+        public Fleet()
+        {
+            InitializeComponent();
+        }
+    }
+}
