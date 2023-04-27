bf508ae138c6f976ab3179fb0761b32303f1fa64
t.hinkov23 <t.hinkov23@LABSCIFIPC04>
2023-04-27 09:01:15 +0300

    bfd

--- HinkovT_FinalPr_Sem2/SignUp.xaml.cs
+++ HinkovT_FinalPr_Sem2/SignUp.xaml.cs
@@ -28,9 +28,23 @@ namespace HinkovT_FinalPr_Sem2
 
         private void SignUp_Click(object sender, RoutedEventArgs e)
         {
-            Book book = new Book();
-            book.Show();
-            this.Close();
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\t.hinkov23\Source\Repos\HinkovT_FinalPr_Sem2\HinkovT_FinalPr_Sem2\FinalSem2.mdf;Integrated Security=True");
+            try
+            {
+                sqlCon.Open();
+                string query = "INSERT INTO SignUp (firstname, lastname, username, email, password) values('" + this.txtFirst_name.Text + "','" + this.txtLast_name.Text + "','" + this.txtUsername.Text + "','" + this.txtEmail.Text + "','" + this.PasswordBox.Password + "')";
+                SqlCommand cmd = new SqlCommand(query, sqlCon);
+                cmd.ExecuteNonQuery();
+                Book book = new Book();
+                book.Show();
+                this.Close();
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+            
+            
         }
     }
 }
